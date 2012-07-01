25c7b895437123a98c74fc5d34c9d90051f43691
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-01 16:21:36 +0200

    Assert basic task properties when read from repo

--- test/unit/test_repository.rb
+++ test/unit/test_repository.rb
@@ -8,7 +8,17 @@ class TestRepository < Test::Unit::TestCase
   end
   
   def test_all
-    assert_equal(7, @repo.tasks.size, 'Should not present recurring tasks as separate tasks') 
+    assert_equal(7, @repo.tasks.size) 
+    
+    one = @repo['6fd0ba4a-ab67-49cd-ac69-64aa999aff8a']
+    assert_equal('Select a free weekend in November', one.description)
+#    assert_equal('H', one.priority)
+    assert_equal('party', one.project)
+    assert_equal(:pending, one.status)
+
+#    assert_equal(1, one.annotations.size)    
+#    assert_equal(DateTime.new('20120629T191534Z'), one.annotations.first.entry)
+#    assert_equal('the 13th looks good', one.annotations.first.entry.description)
   end
   
   def test_child
