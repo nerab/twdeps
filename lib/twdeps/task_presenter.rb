61192848ef6e8e5f9ed4fbb38ee7cfc7a114c6de
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-05 23:44:13 +0200

    Add comments about presenters and how to use them for global node ordering

--- lib/twdeps/task_presenter.rb
+++ lib/twdeps/task_presenter.rb
@@ -11,6 +11,11 @@ module TaskWarrior
       def attributes
         attrs = {:label => @task.description}
         attrs.merge!({:tooltip => "Status: #{@task.status}"})
+
+        # TODO Once we see the urgency in the JSON export, we can color-code the nodes
+        # http://taskwarrior.org/issues/973
+        # attrs.merge!({:fillcolor => 'red', :style => 'filled'})
+
         attrs.merge!({:fontcolor => 'gray', :color => 'gray'}) if :completed == @task.status
         attrs
       end
